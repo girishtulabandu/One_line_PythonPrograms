3dda094575bedc009154bf124735edc8f2efa873
Arjun Prasad Sarkhel <ap.sarkhel@gmail.com>
2020-10-01 06:36:38 -0600

    Created reverse of a string one-liner
    Created one liner for reverse of a string with comment.

---
+++ reverse_string.py
@@ -0,0 +1,3 @@
+#Here we use string slicing to reverse a string, just replace the variable stringname with the string you want to reverse.
+#for example print("car"[::-1]) will result in rac.
+stringname[::-1]
