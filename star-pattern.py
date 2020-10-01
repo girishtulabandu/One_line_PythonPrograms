527bc9380c28d8b0cbaeb20a7b91b9615d89b14f
GIRISH <girishtulabandu66@gmail.com>
2020-10-01 14:11:34 +0530

    Update star-pattern.py

--- star-pattern.py
+++ star-pattern.py
@@ -5,5 +5,7 @@ Python One Liner for a star pattern
 * * *                                                                                                                                  
 * * * *                                                                                                                                
 * * * * * 
+
+Input format-> pattern(5)
 """
 pattern = lambda x: '\n'.join('* ' * i for i in range(x+1))
