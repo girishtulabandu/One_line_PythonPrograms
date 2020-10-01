c4ac45a851a2663b7a413675f14b1bac9b69bbac
n00bi3D00 <ee160002017@iiti.ac.in>
2020-10-01 17:36:05 +0530

    added grep-regex.py

--- grep-regex.py
+++ grep-regex.py
@@ -1,1 +1,2 @@
-# grep command is used to filter the lines out of a file, and we can do this by having a variable named line (to represent the current line of some file) 
+# grep command is used to filter the lines out of a file, and we can do this by having a variable named line (to represent the current line of some file), we will use this technique to find the first name of the author of a book (we assume the name of the author is mentioned is the book)and also the regular expression library 're' is imported.
+re.findall('Author:.*(\S+)', line)
