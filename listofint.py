1d36e5389f49ef9b5d6502026bc4e7cdf568537e
Maitreyi88 <maitreyi.joshi882001@gmail.com>
2020-10-01 20:37:20 +0530

    onelinepy

---
+++ listofint.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+#shows the list only upto the input number
+list(map(int,input("Enter the list elements:").split()[:int(input("enter the number: "))]))
+
---
+++ listofstr.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+#prints the list of strings
+'\n'.join(str(line) for line in (str,input("Enter the list elements:").split()))
+
