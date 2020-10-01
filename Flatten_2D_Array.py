13f72dd289bd9e38713ce17773c76a71b2381548
bilwa496 <66560100+bilwa496@users.noreply.github.com>
2020-10-01 16:08:09 +0530

    Update Flatten_2D_Array.py
    Co-authored-by: GIRISH <girishtulabandu66@gmail.com>

--- Flatten_2D_Array.py
+++ Flatten_2D_Array.py
@@ -2,4 +2,4 @@
 #Suppose there is a 2D matrix- [[1, 2], [3, 4], [5, 6]]
 # It will be turned into a 1-dimensional array ,ie. prints [1, 2, 3, 4, 5, 6]
 
-ls = [[1, 2], [3, 4], [5, 6]]print([i for j in ls for i in j]) 
+flattened_list = [y for x in 2d_array for y in x]
