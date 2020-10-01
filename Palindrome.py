53b9c9ce87d3d5d498403406eb682281eed58169
Deepak Singh <malwareperseus@gmail.com>
2020-10-01 13:07:56 +0530

    Created Palindrome Checker

---
+++ Palindrome.py
@@ -0,0 +1,2 @@
+# Palindrome Finder (Eg-> if racecar is passed as the Phrase {racecar (It's the same, read from both the sides)} will give 0, if not a Palindrome will give -1)
+phrase.find(phrase[::-1])
