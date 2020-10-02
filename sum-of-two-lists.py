01793b736926b1f4a02a1e851eaab818f9634424
abhishektandon <35254972+abhishektandon@users.noreply.github.com>
2020-10-02 15:13:01 +0530

    Update sum-of-two-lists.py

--- sum-of-two-lists.py
+++ sum-of-two-lists.py
@@ -1,5 +1,6 @@
 # This python one-liner takes two list and returns a single list 
 # containing sum of elements index wise i.e. new_list[0] = list1[0] + list2[0], and so...
-# example: [1, 2, 3, 4, 5] + [10, 11, 12, 13, 14] = [11, 13, 15, 17, 19]
+# input: list1 = [1, 2, 3, 4, 5], list2 = [10, 11, 12, 13, 14] 
+# output: [11, 13, 15, 17, 19]
 
 new_list = list(map(lambda x,y:x+y,range(1,6),range(10,16))
