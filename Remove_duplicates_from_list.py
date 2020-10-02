123640862f9ebeeacf207c7704c1e0d764af5b94
GIRISH <girishtulabandu66@gmail.com>
2020-10-02 13:58:33 +0530

    Update and rename Remove_duplicates_from_list.py to remove_duplicates_from_list.py (#66)

--- Remove_duplicates_from_list.py
+++
@@ -1,2 +0,0 @@
-#Remove duplicates from a list Sample Input:[1,1,2,4,2,1,7,8,6,7,8]  Output:[1,2,4,8,6,7]
-unique_list = lambda listt : list(set(listt))
---
+++ remove_duplicates_from_list.py
@@ -0,0 +1,6 @@
+'''
+Remove duplicates from a list 
+Sample Input:[1,1,2,4,2,1,7,8,6,7,8] 
+Output:[1,2,4,8,6,7]
+'''
+unique_list = lambda sample_list : list(set(sample_list))
