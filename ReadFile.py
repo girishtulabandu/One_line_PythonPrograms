bb96f25e10ce8124f41aa2aa33fe9da931fed739
bilwa496 <66560100+bilwa496@users.noreply.github.com>
2020-10-02 10:49:54 +0530

    Prepend the string to each number in an ordered manner- One Line Python code (#32)

---
+++ ReadFile.py
@@ -0,0 +1,3 @@
+ # To read a file from the console- in one-line code only, filename is the name of the file below- everything in one line code.
+ 
+[line.strip() for line in open(filename)]
---
+++ sum_of_odd.py
@@ -0,0 +1,2 @@
+#sum of odd from 1 to n
+sumodd = sum([x for x in range(n) if x%2==1])
