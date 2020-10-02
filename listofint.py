3f2274a813ecf4fd464e82526665e063588e377f
Maitreyi88 <61378749+Maitreyi88@users.noreply.github.com>
2020-10-02 12:38:07 +0530

    Update listofint.py

--- listofint.py
+++ listofint.py
@@ -4,6 +4,6 @@
 # In[10]:
 
 
-#shows the list only upto the input number eg.input list=3 2 3 5 5 6 7 ,input number=4 output=[3,2,3,5]
+#shows the list of integers only upto the input number eg.input list=3 2 3 5 5 6 7 ,input number=4 output=[3,2,3,5]
 list(map(int,input("Enter the list elements:").split()[:int(input("enter the number: "))]))
 
