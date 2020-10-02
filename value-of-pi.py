c1a14b2c5d86a1dc012be985144c8cefd80233ae
abhishektandon <35254972+abhishektandon@users.noreply.github.com>
2020-10-02 15:11:38 +0530

    Update value-of-pi.py

--- value-of-pi.py
+++ value-of-pi.py
@@ -1,2 +1,5 @@
 # Python one-liner to print the value of pi
-4*sum((-1.0)**(n%2) / (2*n + 1) for n in range(2010))
+# input: no input required
+# output: 3.1410951411827663
+
+value_of_pi = 4*sum((-1.0)**(n%2) / (2*n + 1) for n in range(2010))
