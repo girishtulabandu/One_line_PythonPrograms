6c6d857927acd22a36c272680ff40bc907758c88
André Decher <andre.decher@gmail.com>
2020-10-02 13:32:55 +0200

    comment with sample input and expected output

--- DecodeHexString.py
+++ DecodeHexString.py
@@ -1,2 +1,2 @@
-# Decode string written in Hex, prints GNU's Not Unix
-print(''.join(chr(int(''.join(i), 16)) for i in zip(*[iter('474e552773204e6f7420556e6978')]*2)))
+# Decode string written in Hex, input = 4861707079204861636b746f62657266657374 output = Happy Hacktoberfest
+print(''.join(chr(int(''.join(i), 16)) for i in zip(*[iter('4861707079204861636b746f62657266657374')]*2)))
