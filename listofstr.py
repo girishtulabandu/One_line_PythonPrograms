527dc3d526b83636cbf5caf7cbe1366187ff1aac
Maitreyi88 <61378749+Maitreyi88@users.noreply.github.com>
2020-10-02 12:37:31 +0530

    Update listofstr.py

--- listofstr.py
+++ listofstr.py
@@ -4,6 +4,6 @@
 # In[22]:
 
 
-#prints the list of strings
+#prints the list of strings e.g. input list=hello how r you ,output=['hello','how','r','you']
 '\n'.join(str(line) for line in (str,input("Enter the list elements:").split()))
 
