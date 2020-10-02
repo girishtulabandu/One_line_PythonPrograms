9c2165d0c8d23b1b4edaae3e960ae1a2cc059e8f
GIRISH <girishtulabandu66@gmail.com>
2020-10-02 10:53:55 +0530

    Update and rename evenNumber.py to print_even_numbers.py

--- evenNumber.py
+++
@@ -1,2 +0,0 @@
-#program to print even number in any given range
-evenNumbers =[x for x in range(11) if x % 2 == 0]
---
+++ print_even_numbers.py
@@ -0,0 +1,2 @@
+#program to print even number in any given range n
+evenNumbers =[x for x in range(n) if x % 2 == 0]
