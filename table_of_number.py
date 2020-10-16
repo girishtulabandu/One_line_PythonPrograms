5bc17f47c3b81c16483a7325c14fc33cdfc634fe
Om Kumar <65042638+omkumar40@users.noreply.github.com>
2020-10-16 16:53:45 +0530

    created table_of_number.py (#50)

---
+++ table_of_number.py
@@ -0,0 +1,4 @@
+#It will print the table of a given number in place of num(add any number)
+#Input : Replace num(eg:2) with any number
+#Output: 2 4 6 8 10 12 14 16  19 20(in a seperate line)
+x=[print(num*i) for i in range(1,11)]
