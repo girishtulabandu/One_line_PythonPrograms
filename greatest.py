5eee9a72379f03872bdc9ee05af3600e4865855e
Sejal-Srivastava <sejalshrivastav51@gmail.com>
2020-10-16 16:56:27 +0530

    Create greatest.py (#52)

---
+++ greatest.py
@@ -0,0 +1,9 @@
+#to find largest number among three numbers using Ternary operator
+greatest=(x if x>z else z) if x>y else (y if y>z else z)
+
+#sample input1
+#x=7
+#y=8
+#z=10
+#sample output1
+#10
