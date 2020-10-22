2f9d717e00c7bba289a27ea0f947029eac854efe
Pratik Thorat <pratikthorat@users.noreply.github.com>
2020-10-22 10:49:32 +0530

    Update CircumferenceOfCircle.py

--- CircumferenceOfCircle.py
+++ CircumferenceOfCircle.py
@@ -1,3 +1,5 @@
-#Input
-#rad - radius of circle
+#Program: To find circumference of a circle with given radius
+#Input: rad - radius of circle
+#Output: circum - circumference of circle with input radius
+#Formula used - 2*PI*radius (where PI - 3.1415) 
 circum = 2 * 3.1415 * rad
